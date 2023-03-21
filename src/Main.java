98388191feb11fde7e6b565a263f7324f0073d69
qmained <66349725+qmained@users.noreply.github.com>
2023-03-22 04:52:20 +1000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.TreeMap;
 public class Main{
 
     enum Roman{
-        I, II, III, IV, V, VI, VII, VIII, IX, X, XI, XII, XIII, XIV, XV, XVI, XVII, XVIII, XIX, XX
+        I, II, III, IV, V, VI, VII, VIII, IX, X
     }
 
     public static class RomanNumber {
